55e04af3e30b40f8ea4f49fe72f21d802bf92089
Michał Buszkiewicz <michal@curiosum.com>
2025-05-09 02:33:57 +0200

    Initial implementation of Permit Absinthe resolvers

--- lib/permit_absinthe.ex
+++ lib/permit_absinthe.ex
@@ -1,18 +1,134 @@
-defmodule PermitAbsinthe do
-  @moduledoc """
-  Documentation for `PermitAbsinthe`.
-  """
+defmodule Permit.Absinthe do
+  use Absinthe.Schema.Notation
+
+  alias Permit.Absinthe.Schema.Meta
 
   @doc """
-  Hello world.
+  Shorthand for adding the `permit` meta to the field. Most importantly, it allows specifying:
+  - the Ecto schema (or resource struct) that a GraphQL type maps to,
+  - the Permit action that the GraphQL field is allowed to perform on the resource.
 
-  ## Examples
+  ## Example
 
-      iex> PermitAbsinthe.hello()
-      :world
+    ```elixir
+    object :article do
+      # Equivalent to: meta(permit: [schema: Blog.Content.Article])
+      permit(schema: Blog.Content.Article)
+    end
 
+    field :articles, list_of(:article) do
+      # Equivalent to: meta(permit: [action: :read])
+      permit(action: :read)
+    end
+    ```
   """
-  def hello do
-    :world
+  defmacro permit(opts) do
+    quote do
+      meta(permit: unquote(opts))
+    end
+  end
+
+  @doc """
+  Absinthe resolver that loads and authorizes a single resource.
+
+  ## Example
+
+    ```elixir
+    field :article, :article do
+      permit(action: :show)
+
+      resolve(&load_and_authorize_one/3)
+    end
+    ```
+  """
+  def load_and_authorize_one(authorization_module, parent, args, resolution) do
+    load_and_authorize(:one, authorization_module, parent, args, resolution)
+  end
+
+  @doc """
+  Absinthe resolver that loads and authorizes a list of resources.
+
+  ## Example
+
+    ```elixir
+    field :articles, list_of(:article) do
+      permit(action: :index)
+
+      resolve(&load_and_authorize_all/3)
+    end
+    ```
+  """
+  def load_and_authorize_all(authorization_module, parent, args, resolution) do
+    load_and_authorize(:all, authorization_module, parent, args, resolution)
+  end
+
+  defp load_and_authorize(arity, authorization_module, _parent, args, resolution)
+       when arity in [:one, :all] do
+    type_meta = Meta.get_type_meta_from_resolution(resolution, :permit)
+    field_meta = Meta.get_field_meta_from_resolution(resolution, :permit)
+
+    module = type_meta[:schema]
+    action = field_meta[:action]
+
+    case authorization_module.resolver_module().resolve(
+           resolution.context[:current_user],
+           authorization_module,
+           module,
+           action,
+           %{params: args, resolution: resolution, base_query: &base_query/1},
+           arity
+         ) do
+      {:authorized, resource} ->
+        {:ok, resource}
+
+      :unauthorized ->
+        {:error, "Unauthorized"}
+
+      :not_found ->
+        {:error, "Not found"}
+    end
+  end
+
+  defp base_query(%{
+         resource_module: resource_module,
+         resolution: resolution,
+         params: params
+       }) do
+    field_meta = Meta.get_field_meta_from_resolution(resolution, :permit)
+    param = field_meta[:id_param_name] || :id
+    field = field_meta[:id_struct_field_name] || :id
+
+    case params do
+      %{^param => id} ->
+        resource_module
+        |> Permit.Ecto.filter_by_field(field, id)
+
+      _ ->
+        Permit.Ecto.from(resource_module)
+    end
+  end
+
+  defmacro __using__(opts) do
+    quote do
+      import unquote(__MODULE__)
+
+      def load_and_authorize_one(parent, args, resolution) do
+        unquote(__MODULE__).load_and_authorize_one(
+          unquote(opts[:authorization_module]),
+          parent,
+          args,
+          resolution
+        )
+      end
+
+      def load_and_authorize_all(parent, args, resolution) do
+        unquote(__MODULE__).load_and_authorize_all(
+          unquote(opts[:authorization_module]),
+          parent,
+          args,
+          resolution
+        )
+      end
+    end
   end
 end
---
+++ lib/schema/meta.ex
@@ -0,0 +1,39 @@
+defmodule Permit.Absinthe.Schema.Meta do
+  @doc """
+  Extracts meta information from a field's return type in a resolver.
+
+  ## Examples
+
+      def resolve_article(_, _, resolution) do
+        meta = get_type_meta_from_resolution(resolution)
+        # meta will contain %{schema: Blog.Content.Article}
+        # ...
+      end
+  """
+  def get_type_meta_from_resolution(resolution, meta_keys) when is_list(meta_keys) do
+    type =
+      case resolution.definition.schema_node.type do
+        %{of_type: inner_type} -> inner_type
+        other -> other
+      end
+      |> resolution.schema.__absinthe_type__()
+
+    meta = type.__private__[:meta] || []
+
+    get_in(meta, meta_keys)
+  end
+
+  def get_type_meta_from_resolution(resolution, meta_key) when is_atom(meta_key) do
+    get_type_meta_from_resolution(resolution, [meta_key])
+  end
+
+  def get_field_meta_from_resolution(resolution, meta_keys) when is_list(meta_keys) do
+    meta = resolution.definition.schema_node.__private__[:meta] || []
+
+    get_in(meta, meta_keys)
+  end
+
+  def get_field_meta_from_resolution(resolution, meta_key) when is_atom(meta_key) do
+    get_field_meta_from_resolution(resolution, [meta_key])
+  end
+end
