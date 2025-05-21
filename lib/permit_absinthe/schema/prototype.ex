425d6e78b742ff7c86ad2dc188a606d578e8f18e
Michał Buszkiewicz <michal@curiosum.com>
2025-05-21 17:42:09 +0200

    Introduce :authorize directive

---
+++ lib/permit_absinthe/schema/prototype.ex
@@ -0,0 +1,21 @@
+defmodule Permit.Absinthe.Schema.Prototype do
+  use Absinthe.Schema.Prototype
+
+  directive :authorize do
+    on([:field_definition, :object])
+
+    description("Authorizes a field")
+
+    expand(fn _args, node ->
+      existing = node.middleware || []
+
+      arity =
+        case node.type do
+          %Absinthe.Blueprint.TypeReference.List{} -> :all
+          _ -> :one
+        end
+
+      %{node | middleware: [{Permit.Absinthe.Middleware.LoadAndAuthorize, arity} | existing]}
+    end)
+  end
+end
