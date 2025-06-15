c1b3dc71aea2c832fac8baee6594b9e20488aa6a
Michał Buszkiewicz <michal@curiosum.com>
2025-06-16 01:05:15 +0200

    feat: add custom dataloader module

---
+++ lib/permit_absinthe/dataloader.ex
@@ -0,0 +1,24 @@
+defmodule Permit.Absinthe.Dataloader do
+  import Ecto.Query
+
+  @spec new(
+          module(),
+          Permit.Types.authorization_module(),
+          Permit.Types.subject(),
+          Permit.Types.action_group()
+        ) :: Dataloader.Ecto.t()
+  def new(repo, authorization_module, subject, action) do
+    Dataloader.Ecto.new(repo,
+      query: fn queryable, _params ->
+        # Fix this in Permit.Ecto so this case is not needed
+        case authorization_module.accessible_by!(subject, action, queryable) do
+          %Ecto.Query.DynamicExpr{} = expr ->
+            from(q in queryable, where: ^expr)
+
+          query ->
+            query
+        end
+      end
+    )
+  end
+end
